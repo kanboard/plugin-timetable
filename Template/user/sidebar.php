50ced606e27acc78a0ceea214ca6d6ccb8fb5db5
Kasun Fernando <kasun.elink@gmail.com>
2016-06-28 06:00:35 +0000

    Add files via upload
    Allow Managing Time Table User Itself.

--- Template/user/sidebar.php
+++ Template/user/sidebar.php
@@ -1,4 +1,4 @@
-<?php if ($this->user->isAdmin()): ?>
+<?php if ($this->user->isUser()): ?>
     <li <?= $this->app->getRouterController() === 'timetable' ? 'class="active"' : '' ?>>
         <?= $this->url->link(t('Manage timetable'), 'timetable', 'index', array('plugin' => 'timetable', 'user_id' => $user['id'])) ?>
     </li>
